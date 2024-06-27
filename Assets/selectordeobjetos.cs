71c05e927cad3a395b2b7fe3984e55c8139c022d
FacuMG01 <163903696+FacuMG01@users.noreply.github.com>
2024-06-27 10:18:19 -0300

    .

--- Assets/selectordeobjetos.cs
+++ Assets/selectordeobjetos.cs
@@ -5,15 +5,5 @@ using UnityEngine;
 public class selectordeobjetos : MonoBehaviour
 {
     public int precios; 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
